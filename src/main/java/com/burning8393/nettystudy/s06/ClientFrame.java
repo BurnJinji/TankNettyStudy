b754dfa7583ea03cab8c5f7e0ddcf71604f50d48
DESKTOP-CRI8Q20\Pang <pang798763716@outlook.com>
2019-11-25 19:34:35 +0800

    client端接收到channelRead后更新 把ClientFrame做成单例

---
+++ src/main/java/com/burning8393/nettystudy/s06/ClientFrame.java
@@ -0,0 +1,40 @@
+package com.burning8393.nettystudy.s06;
+
+import java.awt.*;
+
+public class ClientFrame extends Frame {
+    public final static ClientFrame INSTANCE = new ClientFrame();
+    TextArea ta = new TextArea();
+    TextField tf = new TextField();
+
+    Client client = null;
+    public ClientFrame() throws HeadlessException {
+        this.setSize(600, 400);
+        this.setLocation(50, 20);
+        this.add(ta, BorderLayout.CENTER);
+        this.add(tf, BorderLayout.SOUTH);
+
+        tf.addActionListener(e -> {
+            client.send(tf.getText());
+            ta.setText(ta.getText() + System.getProperty("line.separator") +  tf.getText());
+            tf.setText("");
+        });
+
+
+    }
+
+    public void connectToServer() {
+        client = new Client();
+        client.connect();
+    }
+
+    public void updateText(String acceptMsg) {
+        this.ta.setText(ta.getText() + System.getProperty("line.separator") + acceptMsg);
+    }
+
+    public static void main(String[] args) {
+        ClientFrame frame = ClientFrame.INSTANCE;
+        frame.setVisible(true);
+        frame.connectToServer();
+    }
+}
---
+++ src/main/java/com/burning8393/nettystudy/s06/Server.java
@@ -0,0 +1,68 @@
+package com.burning8393.nettystudy.s06;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+public class Server {
+    public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup(2);
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            ChannelFuture f = b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new ServerChildHandler());
+                        }
+                    })
+                    .bind(8888)
+                    .sync();
+
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+
+
+    }
+}
+
+class ServerChildHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Server.clients.add(ctx.channel());
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = null;
+        try {
+            buf = (ByteBuf) msg;
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(), bytes);
+            System.out.println(new String(bytes));
+            Server.clients.stream().filter(c -> c != ctx.channel()).forEach(c -> c.writeAndFlush(msg));
+        } finally {
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
